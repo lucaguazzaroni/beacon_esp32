690a2986cc7177bd7908380959f7c6908b88e914
luca guazzaroni <lucaguazzaroni@gmail.com>
2018-06-18 09:30:25 -0300

    add use of eddytone uuid on main

--- main/esp_beacon.c
+++ main/esp_beacon.c
@@ -22,12 +22,16 @@
 
 #include "beacon_ble_api.h"
 #include "esp_altbeacon_api.h"
+#include "esp_eddystone_api.h"
 #include "esp_ibeacon_api.h"
 
 static const char* TAG = "MAIN";
 
 uint8_t test_uuid[] = {0xFD, 0xA5, 0x06, 0x93, 0xA4, 0xE2, 0x4F, 0xB1, 0xAF, 0xCF, 0xC6, 0xEB, 0x22, 0x22, 0x22, 0x22};
 uint8_t test_uuid_extra[] = {0x11,0x11,0x00,0x00};
+
+uint8_t test_namespace[] = {0xFD, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99};
+uint8_t test_instance[] = {0x10, 0x20, 0x30, 0x10, 0x20, 0x30};
 
 void app_main()
 {
@@ -37,7 +41,7 @@ void app_main()
     beacon_ble_init();
 
     /* Config beacon_ble_api to work as an advertiser */
-    beacon_ble_config(BEACON_MODE_ADVERTISER, BEACON_TYPE_ALTBEACON);
+    beacon_ble_config(BEACON_MODE_ADVERTISER, BEACON_TYPE_EDDYSTONE_UUID);
     
     /* set scan parameters */
     if( beacon_is_scanner() )
@@ -74,6 +78,16 @@ void app_main()
                 advertising_packet = (uint8_t *)malloc(advertising_packet_size); 
                 // get advertising data from altbeacon_api 
                 ibeacon_get_adv_data(advertising_packet); 
+                break;
+            case BEACON_TYPE_EDDYSTONE_UUID:
+                // if eddystone_config_data is not called, default values are going to be published when beacon_advertiser_start() is called
+                eddystone_config_data(test_namespace, test_instance, 0xC3); 
+                // get size of adv data
+                advertising_packet_size = eddystone_get_adv_data_size(); 
+                // create array to store the advertising data
+                advertising_packet = (uint8_t *)malloc(advertising_packet_size); 
+                // get advertising data from altbeacon_api 
+                eddystone_get_adv_data(advertising_packet);
                 break;
             default:
                 break;
